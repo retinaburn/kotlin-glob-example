95887ab1acef46e57632731566acda230b226346
Craig Moynes <cmoynes@ca.ibm.com>
2017-08-25 14:02:10 -0400

    Initial commit

---
+++ glob-traverse.kt
@@ -0,0 +1,51 @@
+import java.io.File
+import java.nio.file.PathMatcher
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/**
+*
+* Take a list of files and/or globs on the command line and perform some action
+*
+**/
+fun main(args: Array<String>){
+  if (args.isEmpty()){
+    println(usage())
+  }
+  for (arg in args){
+    val filename = arg
+    val f = File(filename)
+
+    if (f.exists()){
+      handleFile(f)
+    } else {
+      val ds = Files.newDirectoryStream(f.getParentFile().toPath(), f.getName())
+
+      for(path in ds){
+        handleFile(path.toFile())
+      }
+    }
+  }
+}
+
+/*
+* Do something interesting with the file
+*/
+fun handleFile(f: File){
+  println(f)
+}
+
+fun usage(): String{
+  return """
+  Usage: java -jar glob-traverse.jar [file(s)] [glob(s)]
+    file(s) is a file or list of file(s)
+    glob(s) is a glob or list of glob(s)
+
+      Example:
+       java -jar glob-traverse.jar sampleFile.txt")
+       java -jar glob-traverse.jar sampleFile.txt sample2.zip sample3.zip")
+       java -jar glob-traverse.jar sampleFile.txt ../*.zip")
+       java -jar glob-traverse.jar /path/somewhere/fun/2017-*")
+""".trimIndent()
+}
